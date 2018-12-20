71dc6dd58f99bdbb710ada850d3674ddf1d1ab02
Adam Burton <adamburton27@hotmail.com>
2018-12-20 16:15:50 +0000

    comit

---
+++ main.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const nunjucks = require("nunjucks");
+const sassMiddleware = require("node-sass-middleware");
+const axios = require("axios");
+const path = require("path");
+
+var app = express();
+
+// Compile sass files to css
+app.use(sassMiddleware({
+    src: path.join(__dirname, 'public/sass'),
+    dest: path.join(__dirname, 'public/css'),
+    debug: true,
+    sourceMap: true,
+    indentedSyntax : false,
+    outputStyle: 'compressed'
+  })); 
+
+// Declare assets location
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Page template configuration
+var PATH_TO_TEMPLATES = './pages' ;
+nunjucks.configure( PATH_TO_TEMPLATES, {
+    autoescape: true,
+    express: app
+});
+
+// routes
+app.get( '/', function( req, res ) {
+    return res.render( 'index.html') ;
+});
+
+app.get( '/search', function( req, res ) {
+
+});
+
+app.get( '/asset', function( req, res ) {
+
+});
+
+// run local server
+app.listen( 3000 );
