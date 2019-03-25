3c47d81918169f5301644491e86fbca74fc94902
Adam Burton <adamburton27@hotmail.com>
2019-03-25 11:20:24 +0000

    lots

--- controllers/homepageController.js
+++ controllers/homepageController.js
@@ -1,3 +1,14 @@
+const documentService = require("../services/documentService");
+
 exports.home = function(req,res){
-    return res.render( 'index.html');
+    
+  var documentTypes = [];
+
+  documentTypes.push({"name":"Roundabout sponsorship","reference":"roundabout-sponsorship","tagline":"Over 80 roundabouts in high traffic areas"});
+  documentTypes.push({"name":"Bin lid advertising","reference":"bin-lid-advertising","tagline":"Unique and highly targeted"});
+  documentTypes.push({"name":"Car Parking Ticket Advertising","reference":"car-parking-ticket-advertising","tagline":"Directly into the hands of your audience"});
+  documentTypes.push({"name":"Vehicle Advertising","reference":"vehicle-advertising","tagline":"Create a moving billboard"});
+  documentTypes.push({"name":"Bus Shelter Posters","reference":"bus-shelter-posters","tagline":"Prime seafront advertising in Weymouth"});
+  documentTypes.push({"name":"Boundary Signage Sponsorship","reference":"boundary-signage-sponsorship","tagline":"Welcome to Dorset, home of 'your business'"})
+    return res.render( 'index.html',{"documentTypes":documentTypes});
 };
